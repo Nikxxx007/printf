151a3f005111fe777c1faed6227ca4fae0428bbd
Nick Nefed <nicknefed@MacBook-Pro-Nick.local>
2021-07-16 13:49:13 +0300

    finished

--- main.c
+++ main.c
@@ -8,7 +8,8 @@ int main()
     int i = -2147483648;
     int k = 15;
 //    int len = ft_printf("%c %s %p %d %i %u %x %X %%\n", a, str, &str, i, i, u_i, k, k);
-    int len = ft_printf("%s %c\n", str, a);
+    int len = ft_printf("%s\n", str);
+	printf("%d\n", len);
     len = printf("%s\n", str);
     printf("%d\n", len); // if str == NULL
 	return 0;
--- src/print/print_string.c
+++ src/print/print_string.c
@@ -5,5 +5,8 @@ void	print_string(t_procent *pr, va_list list)
 	char	*value;
 
 	value = va_arg(list, char *);
-	ft_putstr_fd(value, 1, pr);
+	if (value == NULL)
+		pr->len += write(1, "(null)", 6);
+    else
+    	ft_putstr_fd(value, 1, pr);
 }
